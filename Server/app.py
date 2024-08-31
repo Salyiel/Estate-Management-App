9129d98fa1a9d4d62f44456303753524b7be8740
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-08-31 14:49:32 +0300

    App code

--- Server/app.py
+++ Server/app.py
@@ -1,3 +1,4 @@
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
@@ -9,7 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Define models here
+# Define your models here
 
 if __name__ == '__main__':
     app.run(debug=True)
